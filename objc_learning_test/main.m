eaa398454310bcdd0ff3a34f3311a0d56d91fb88
daicl <dcl.outlook.1016@gmail.com>
2023-04-03 21:42:21 +0800

    Add; objc_learning_test

---
+++ objc_learning_test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  objc_learning_test
+//
+//  Created by daicanglan on 2023/4/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObject *obj = [[NSObject alloc] init];
+        NSLog(@"Test objc4 learning....");
+    }
+    return 0;
+}
+
